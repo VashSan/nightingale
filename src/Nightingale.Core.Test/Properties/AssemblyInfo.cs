0cae8eca8f51e4490c4b8fc7f5778dcd8a170d99
Vash <alex@palc.de>
2020-11-14 22:16:50 +0100

    added core lib project

---
+++ src/Nightingale.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Nightingale.Core.Test" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Nightingale.Core.Test" )]
+[assembly: AssemblyCopyright( "Copyright ©  2020" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "6d8618b6-07e2-47ae-8b86-9113b1e64b87" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ src/Nightingale.Core.Test/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Nightingale.Core.Test
+{
+	[TestFixture]
+	class Test
+	{
+		[Test]
+		public void FirstTest()
+		{
+			Assert.That( true, Is.True );
+		}
+	}
+}
